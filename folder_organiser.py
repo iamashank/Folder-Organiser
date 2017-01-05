0756c7961b8f7c72f303efef11e29400205e3b76
Ashank Anshuman <ashankanshuman@gmail.com>
2017-01-06 00:29:19 +0530

    Update folder_organiser.py

--- folder_organiser.py
+++ folder_organiser.py
@@ -1,7 +1,7 @@
 import os
 import re
 import shutil, stat, sys
-if (len(sys.argv)==1):
+if (len(sys.argv)<2):
     source = 'C:\\Users\\Ashank\\Downloads'                 #Directory to be organised
 else:
     source = sys.argv[1]
